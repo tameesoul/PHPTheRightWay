721a7f89beb59fcee186674837b7b7e93fb12e00
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2024-07-09 01:19:46 +0300

    01 basicPHPsyntax

---
+++ 01BasicPHP.php
@@ -0,0 +1,28 @@
+<?php
+// print
+echo "hello,world"; /// one way to print , most efficient
+echo "<br>";
+print "hello,world"; /// other way to print , return 1 ;
+
+// variable declaration
+
+$name = "ahmed";
+echo "<br>";
+echo 'hello'.' '.$name;
+
+echo "<br>";
+// value vs reference 
+
+$x = 10;
+$y = $x;
+$x = 20;
+echo $y; /// 10 assigned by value;
+echo "<br>";
+$x = 10;
+$y = &$x;
+$x = 20;
+echo $y; /// 20 assigned by reference;
+echo "<br>";
+
+
+
